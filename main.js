7205ef00b2c227fc23d9eb6c76e227ada7b42ee0
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-10-23 14:54:24 -0700

    function to get data from API for Rortnite game players

--- main.js
+++ main.js
@@ -2,3 +2,41 @@ $(document).ready(init);
 function init () {
 
 }
+
+var fortniteTopPlayers= [
+    {name: 'Ninja', gtag: 'Ninja'},
+    {name: 'NickMercs', gtag: 'NICKMERCS'},
+    {name: 'TwitchProspering,  gtag: TwitchProspering' },
+    {name: 'twitch_bogdanakh', gtag: 'twitch_bogdanakh'},
+    {name: 'TSM_Myth', gtag: 'TSM_Myth'},
+    {name: 'CourageJD', gtag: 'CourageJD'},
+
+]
+console.log (fortniteTopPlayers);
+
+var fortnitePlayersData = [];
+
+function detFortnitePlayerData(playerName) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://danielpaschal.com/lfzproxies/fortniteproxy.php",
+        "method": "GET",
+        dataType: 'json',
+        data:{
+            platform: 'pc',
+            player: playerName,
+        },
+        "headers": {
+            "TRN-Api-Key": "516f26d7-878d-41f9-b03e-df2ad5a11530",
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        apiCallDataForTwitchProspering = response;
+    });
+}
+
+
+
